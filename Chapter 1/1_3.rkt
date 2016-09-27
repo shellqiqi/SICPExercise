a49cb9a1de5dd0fa202a5d64dac7f94ae0c97461
shellqiqi <shellqiqiX@gmail.com>
2016-09-28 00:40:06 +0800

    pretty .rkt

--- Chapter 1/1_3.rkt
+++ Chapter 1/1_3.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname 1_3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
-;Exercise 1.3
+;; Exercise 1.3
 (define (square x) (* x x))
 (define (sum-of-squares x y)
     (+ (square x) (square y)))
